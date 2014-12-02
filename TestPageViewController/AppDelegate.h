acd456eaaa912b83037b49b18b9c4f26421377fa
谢强强 <xieqiangqiang@shopex.cn>
2014-12-02 11:33:34 +0800

    Initial Commit

---
+++ TestPageViewController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestPageViewController
+//
+//  Created by SunSet on 14-12-2.
+//  Copyright (c) 2014年 SunSet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestPageViewController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestPageViewController
+//
+//  Created by SunSet on 14-12-2.
+//  Copyright (c) 2014年 SunSet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
